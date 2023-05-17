36d9b21b7fcf1b66a8f189206f6ec9de53c1dee1
5533Robotics <5533ehprobotics@gmail.com>
2023-05-16 20:03:16 -0400

    Praise david

--- robot.py
+++ robot.py
@@ -2,37 +2,45 @@ import wpilib
 import wpilib.drive
 
 
-# class Dani(wpilib.TimedRobot):
+class Dani(wpilib.TimedRobot):
+    def robotInit(self):
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+        print("hello from setup")
+        # self.left_motor = wpilib.Spark(0)
+        # self.right_motor = wpilib.Spark(1)
+        # self.drive = wpilib.drive.DifferentialDrive(self.left_motor, self.right_motor)
+        self.spark = wpilib.Spark(1)
+        self.sparkt = wpilib.Spark(0)
+        self.stick = wpilib.Joystick(0)
+        self.timer = wpilib.Timer()
 
-#     def robotInit(self):
-#         """
-#         This function is called upon program startup and
-#         should be used for any initialization code.
-#         """
-#         self.left_motor = wpilib.Spark(0)
-#         self.right_motor = wpilib.Spark(1)
-#         self.drive = wpilib.drive.DifferentialDrive(self.left_motor, self.right_motor)
-#         self.stick = wpilib.Joystick(1)
-#         self.timer = wpilib.Timer()
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
+        self.timer.reset()
+        self.timer.start()
 
-#     def autonomousInit(self):
-#         """This function is run once each time the robot enters autonomous mode."""
-#         self.timer.reset()
-#         self.timer.start()
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
 
-#     def autonomousPeriodic(self):
-#         """This function is called periodically during autonomous."""
+        # Drive for two seconds
+        if self.timer.get() < 2.0:
+            self.drive.arcadeDrive(-0.5, -0.5)  # Drive forwards at half speed
+        else:
+            self.drive.arcadeDrive(0, 0)  # Stop robot
 
-#         # Drive for two seconds
-#         if self.timer.get() < 2.0:
-#             self.drive.arcadeDrive(-0.5, 0)  # Drive forwards at half speed
-#         else:
-#             self.drive.arcadeDrive(0, 0)  # Stop robot
-
-#     def teleopPeriodic(self):
-#         """This function is called periodically during operator control."""
-#         self.drive.arcadeDrive(self.stick.getY(), self.stick.getX())
+    def teleopPeriodic(self):
+        """This function is called periodically during operator control."""
+        # self.right_motor.set(1)
+        # self.left_motor.set(1)
+        #self.drive.arcadeDrive(self.stick.getY(), self.stick.getX())
+        speed = self.stick.getY()
+        self.spark.set(speed)
+        self.sparkt.set(speed)
+        print(f"x:{self.stick.getX()} y:{self.stick.getY()}")
 
 
 if __name__ == "__main__":
-    wpilib.run(wpilib.TimedRobot)
+    wpilib.run(Dani)
