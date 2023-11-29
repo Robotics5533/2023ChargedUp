82153e614b143e67433a5cc7745bae7fe293a681
5533Robotics <5533ehprobotics@gmail.com>
2023-11-28 20:00:55 -0500

    if this breaks everything, i'm sorry

---
+++ constants.py
@@ -0,0 +1,17 @@
+Robot = {
+    "controllers": {
+        "driver": {
+            "joystick": 0
+        },
+        "motors": {
+            "front": {
+                "left": 0,
+                "right": 1
+            },
+            "rear": {
+                "left": 2,
+                "right": 3
+            }
+        }
+    }
+}
