14c191f2437e73d28f4c667f0880f36c8908fdce
5533Robotics <5533ehprobotics@gmail.com>
2023-12-05 18:18:54 -0500

    uwu owo we got a drive class :shy:

--- robot.py
+++ robot.py
@@ -1,7 +1,7 @@
 import wpilib
 import wpilib.drive
-from wpilib.drive import DifferentialDrive
 import ntcore
+from Drive.driveClass import Drive
 from constants import *
 from utils.math import calculate_direction
 import ctre
@@ -13,23 +13,12 @@ class Dani(wpilib.TimedRobot):
         This function is called upon program startup and
         should be used for any initialization code.
         """
-        self.timer = wpilib.Timer()
-        self.frontLeft = wpilib.Talon(0)
-        self.rearLeft = wpilib.Talon(1)
-        self.frontRight = wpilib.Talon(2)
-        self.rearRight = wpilib.Talon(3)
-        self.left = wpilib.MotorControllerGroup(self.frontLeft, self.rearLeft)
-        self.right = wpilib.MotorControllerGroup(self.frontRight, self.rearRight)
-        self.drive = DifferentialDrive(self.left, self.right)
-        self.drive.setExpiration(0.4)
-        self.stick = wpilib.Joystick(1)
-        self.motor1 = ctre.WPI_TalonSRX(1)
-        self.drive.setExpiration(0.2)
-        self.drive.setSafetyEnabled(True)
-        self.drive.feed()
-        # self.ntinst = ntcore.NetworkTableInstance.getDefault()
-        # self.limelightnt = self.ntinst.getTable('limelight')
-        # self.__nt = NetworkTables.getTable("limelight")
+        # self.motor1 = ctre.WPI_TalonSRX(1)
+        # self.drive.setExpiration(0.2)
+        # self.drive.setSafetyEnabled(True)
+        # self.drive.feed()
+        self.drive = Drive("tank")
+
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
@@ -39,16 +28,16 @@ class Dani(wpilib.TimedRobot):
     def autonomousPeriodic(self):
         """This function is called periodically during autonomous."""
         if self.timer.get() < 2.0:
-            self.drive.arcadeDrive(-0.5, -0.5)  # Drive forwards at half speed
+            self.drive.arcadeDrive(-0.5, -0.5)
+              # Drive forwards at half speed
         else:
-            self.drive.arcadeDrive(0, 0)  # Stop robot
+            self.drive.arcadeDrive(0, 0)
+              # Stop robot
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
-        #x, y, z = self.stick.getX(), self.stick.getY(), self.stick.getZ(), 
-        #self.drive.tankDrive(calculate_direction(1, x, y, z), calculate_direction(0, x, y, z))
-        self.motor1.set(0.5)
-
-
+        x, y, z = self.stick.getX(), self.stick.getY(), self.stick.getZ(), 
+        self.drive.move(x, y, z)
+     
 if __name__ == "__main__":
     wpilib.run(Dani)
