4760a4631554a199f8326319c35e0196f38a8609
stormybeat <128640313+stormybeat@users.noreply.github.com>
2023-12-05 19:04:04 -0500

    Create inputs.py

---
+++ inputs/inputs.py
@@ -0,0 +1,10 @@
+import constants
+class Inputs:
+
+    def getValue():
+        return False
+
+class JoyInput(Inputs):
+    pass 
+    def __init__(self, joyName) -> None:
+        super().__init__()
